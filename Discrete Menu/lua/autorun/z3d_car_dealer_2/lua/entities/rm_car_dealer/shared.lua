97c22b712c2549c2528521b0be78e7137556681d
Fortune <fortune11709@gmail.com>
2015-05-09 11:01:54 +1000

    Removed Car Dealer?
    Actually have no idea how/why that was in here.

--- Discrete Menu/lua/autorun/z3d_car_dealer_2/lua/entities/rm_car_dealer/shared.lua
+++
@@ -1,8 +0,0 @@
-ENT.Base = "base_ai"
-ENT.Type = "ai"
-ENT.AutomaticFrameAdvance = true
-ENT.PrintName = "Car Dealer - Extreme"
-ENT.Author = "RocketMania"
-ENT.Category = "RocketMania's"
-ENT.Spawnable = true
-ENT.AdminSpawnable = true
